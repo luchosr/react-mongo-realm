ecfb061ac92c81fa703e119cfab91281ef16eb6c
Luciano Ramello <luchosr@gmail.com>
2022-10-11 14:51:28 +0200

    adds new components

---
+++ src/components/restaurants-list.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RestaurantsList = () => {
+  return <div></div>;
+};
+
+export default RestaurantsList;
---
+++ src/components/restaurants.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Restaurants = () => {
+  return <div></div>;
+};
+
+export default Restaurants;
